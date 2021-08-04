74c54acdabb16bca06b2f2da8ad4b32c283b0961
ViktoriiaVlasiuk <59017270+ViktoriiaVlasiuk@users.noreply.github.com>
2021-08-04 22:52:18 +0300

    Update index.js

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -6,17 +6,18 @@ async function run() {
     const issueTitle = core.getInput("issue-title");
     const jokeBody = core.getInput("joke");
     const token = core.getInput("repo-token");
-
+    
     const octokit = github.getOctokit(token);
-
+    
     const newIssue = await octokit.issues.create({
-      repo: github.context.repo.repo,
-      owner: github.context.repo.owner,
-      title: issueTitle,
-      body: jokeBody
+          repo: github.context.repo.repo,
+          owner: github.context.repo.owner,
+          title: issueTitle,
+          body: jokeBody
     });
-  } catch (error) {
-    core.setFailed(error.message);
+
+  } catch (err) {
+    core.setFailed(err.message);
   }
 }
 
